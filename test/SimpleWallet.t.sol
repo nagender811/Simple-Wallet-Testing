5c79057377e3a6d1dc9ef36715f0690f74b19854
Nagender kumar <kumarnagender811@gmail.com>
2026-06-19 21:26:49 +0530

    Add emergency mode tests and functionality to SimpleWallet

--- test/SimpleWallet.t.sol
+++ test/SimpleWallet.t.sol
@@ -12,6 +12,7 @@ contract SimpleWalletTest is Test {
     address bob;
 
     error Unauthorized();
+    error EmergencyActive();
 
     function setUp() public {
         owner = address(this);
@@ -57,6 +58,7 @@ contract SimpleWalletTest is Test {
         vm.prank(owner);
 
         wallet.transferFromContract(payable(bob), ethersTotransferFromContract);
+
         uint256 balanceOfBobAfter = bob.balance;
         uint256 balanceOfContractAfter = wallet.getContractBalanceInWei();
         uint256 transactionHistoryLengthAfter = wallet.getTransactionHistory().length;
@@ -68,5 +70,62 @@ contract SimpleWalletTest is Test {
         assertEq(transactionHistoryLengthAfter - transactionHistoryLengthBefore, 1);
     }
 
+    function testEmergencyModeBlocksNormalOperations() public {
+        uint256 testStartTime = block.timestamp - 1;
+        vm.prank(owner);
+        wallet.toggleEmergencyMode();
+        
+        vm.deal(alice, 1 ether);
+        vm.prank(alice);
 
+        vm.expectRevert(EmergencyActive.selector);
+        wallet.depositToContract{value: 1 ether}(testStartTime);
+    }
+
+    function testEmergencyWithdrawalTransfersAllFundsToOwner() public {
+        uint256 testStartTime = block.timestamp - 1;
+        uint256 balanceOfOwnerBefore = owner.balance;
+        vm.deal(alice, 10 ether);
+        vm.prank(alice);
+
+        wallet.depositToContract{value: 5 ether}(testStartTime);
+
+        uint256 contractBalanceBeforeWithdrawal = wallet.getContractBalanceInWei();
+
+        vm.prank(owner);
+        wallet.toggleEmergencyMode();
+        wallet.emergencyWithdrawal();
+
+        uint256 balanceOfOwnerAfter = owner.balance;
+
+        uint256 contractBalanceAfterWithdrawal = wallet.getContractBalanceInWei();
+
+        assertEq(contractBalanceAfterWithdrawal, 0);
+        assertEq(balanceOfOwnerAfter - balanceOfOwnerBefore, contractBalanceBeforeWithdrawal);
+    }
+
+    function testTransactionsAreStoredCorrectly() public {
+        uint256 ethersTotransferFromContract = 1 ether;
+        uint256 testStartTime = block.timestamp - 1;
+        vm.deal(alice, 5 ether);
+        vm.prank(alice);
+
+        wallet.depositToContract{value: 3 ether}(testStartTime);
+
+        wallet.transferFromContract(payable(bob), ethersTotransferFromContract);
+
+        SimpleWallet.Transaction[] memory transactions = wallet.getTransactionHistory();
+
+        assertEq(transactions.length, 2);
+        assertEq(transactions[0].sender, alice);
+        assertEq(transactions[0].receiver, address(wallet));
+        assertEq(transactions[0].amount, 3 ether);
+
+        assertEq(transactions[1].sender, address(wallet));
+        assertEq(transactions[1].receiver, bob);
+        assertEq(transactions[1].amount, ethersTotransferFromContract);
+        
+    }
+
+    receive() external payable {}
 }
