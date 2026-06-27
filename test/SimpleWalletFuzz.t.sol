70b51d5795aaca479654b65e880ab06c4a0348f1
Nagender kumar <kumarnagender811@gmail.com>
2026-06-27 15:34:09 +0530

    Add fuzz test for deposit functionality in SimpleWallet

--- test/SimpleWalletFuzz.t.sol
+++ test/SimpleWalletFuzz.t.sol
@@ -21,4 +21,24 @@ contract SimpleWalletFuzzTest is Test {
         wallet = new SimpleWallet();
     }
 
-   
+    function testFuzz_DepositIncreasesContractBalance(
+        uint256 depositAmount
+    ) public {
+        uint256 testStartTime = block.timestamp - 1;
+        depositAmount = bound(depositAmount, 0, 1000 ether);
+        uint256 balanceOfContractBefore = wallet.getContractBalanceInWei();
+        vm.assume(depositAmount != 0);
+        vm.deal(alice, depositAmount);
+
+        vm.prank(alice);
+        wallet.depositToContract{value: depositAmount}(testStartTime);
+
+        uint256 balanceOfContractAfter = wallet.getContractBalanceInWei();
+
+        assertEq(
+            balanceOfContractAfter,
+            balanceOfContractBefore + depositAmount
+        );
+    }
+
+}
