0ff6d03d1108ae484ac7f28bc45415af68a83579
Nagender kumar <kumarnagender811@gmail.com>
2026-07-02 17:10:38 +0530

    Fix import path and add functions for transferring funds in SimpleWalletHandler

--- test/invariant/SimpleWalletHandler.sol
+++ test/invariant/SimpleWalletHandler.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.35;
 
 import {Test} from "forge-std/Test.sol";
-import {SimpleWallet} from "../src/SimpleWallet.sol";
+import {SimpleWallet} from "../../src/SimpleWallet.sol";
 
 contract SimpleWalletHandler is Test {
     SimpleWallet wallet;
@@ -10,6 +10,8 @@ contract SimpleWalletHandler is Test {
     address owner = address(this);
     address alice = makeAddr("alice");
     address bob = makeAddr("bob");
+
+    uint256 previousLength;
 
     constructor(SimpleWallet _wallet) {
         wallet = _wallet;
@@ -22,5 +24,33 @@ contract SimpleWalletHandler is Test {
 
         vm.prank(alice);
         wallet.depositToContract{value: amount}(block.timestamp - 1);
+
+        previousLength++;
+    }
+
+    function transferToOwner(uint256 amount) public {
+        amount = bound(amount, 1, 1000 ether);
+
+        vm.deal(alice, amount);
+        vm.prank(alice);
+
+        wallet.sendFundsToOwner{value: amount}();
+
+        previousLength++;
+    }
+
+    function sendDirectly(address amount) public {
+        amount = bound(amount, 1, 1000 ether);
+
+        vm.deal(alice, amount);
+        vm.prank(alice);
+
+        wallet.transferDirectlyToUser{value: amount}(payable(bob));
+
+        previousLength++;
+    }
+
+    function toggleEmergency() public {
+        wallet.toggleEmergencyMode();
     }
 }
